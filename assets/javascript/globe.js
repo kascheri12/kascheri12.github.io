fe22923e16ca5305f30da20c9944cefd0b2ec7c0
Kenneth Ascheri <ascherik@lanecc.edu>
2018-05-16 01:21:56 -0700

    Add js and new page for Prodider globe

---
+++ assets/javascript/globe.js
@@ -0,0 +1,75 @@
+const width = 800,
+    height = 600,
+    speed = .005,
+    verticalTilt = -20,
+    horizontalTilt = 0;
+let locations = [];
+const svg = d3.select(".globe").append("svg")
+    .attr("width",width)
+    .attr("height",height);
+const markerGroup = svg.append("g");
+
+const projection = d3.geoOrthographic()
+    .scale(245)
+    .translate([width / 2, height / 2])
+    .clipAngle(90);
+const path = d3.geoPath().projection(projection);
+const center = [width/2, height/2];
+const world_map_data_url = "https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/world-110m.json";
+const location_url = "https://kascheri12.github.io/data/";
+
+drawGlobe();
+enableRotation();
+
+function drawGlobe() {
+    d3.queue()
+        .defer(d3.json, world_map_data_url)
+        .defer(d3.csv, location_url)
+        .await((error, worldData, locationData) => {
+            svg.append("path")
+                .datum({type: "Sphere"})
+                .attr("class", "water")
+                .attr("d", path)
+                .style("fill", "#00248F");
+            svg.selectAll(".segment")
+                .data(topojson.feature(worldData, worldData.objects.countries).features)
+                .enter().append("path")
+                .attr("class", "segment")
+                .attr("d", path)
+                .style("stroke", "#FFF")
+                .style("stroke-width", ".7px")
+                .style("fill", (d, i) =>  "#A98B6F")
+                .style("opacity", ".9");
+            locations = locationData;
+            drawMarkers();
+        });
+}
+
+function enableRotation() {
+    d3.timer(function(elapsed) {
+        projection.rotate([speed*elapsed - 120, verticalTilt, horizontalTilt])
+        svg.selectAll("path").attr("d",path);
+        drawMarkers();
+    });
+}
+
+function drawMarkers() {
+  const markers = markerGroup.selectAll("circle")
+      .data(locations)
+  markers
+      .enter()
+      .append("circle")
+      .merge(markers)
+      .attr("cx", d=> projection([d.longitude, d.latitude])[0])
+      .attr("cy", d=> projection([d.longitude, d.latitude])[1])
+      .attr("fill", d=> {
+          const coordinate = [d.longitude, d.latitude];
+          gdistance = d3.geoDistance(coordinate, projection.invert(center));
+          return gdistance > 1.57 ? "none" : "steelblue";
+      })
+      .attr("r", 3);
+
+  markerGroup.each(function() {
+      this.parentNode.appendChild(this);
+  });
+}
---
+++ pages/Provider_Results.md
@@ -0,0 +1,7 @@
+# Provider Results
+
+The graphs of providers that have computed subtasks per my node requests.
+
+<script src="//d3js.org/d3.v4.min.js"></script>
+<script src="//d3js.org/topojson.v1.min.js"></script>
+<script src="{{ base }}/{{ site.javascript_dir }}/globe.js"></script>
