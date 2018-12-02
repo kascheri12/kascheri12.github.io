2fc4b970b021c9c694a46159a0fd8716c75418cf
Kenneth Ascheri <ascherik@lanecc.edu>
2018-12-02 11:49:38 -0800

    automated commit for Golem-Network-Dashboard.md

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
-  $('.top_dt').DataTable({
-    scrollX: "100%",
-    scrollY: 300,
-    searching: false,
-    paging: false,
-    order: [[ 0, "desc" ],[ 3, "desc" ]],
-    bInfo: false
-  });
+  // $('.top_dt').DataTable({
+  //   scrollX: "100%",
+  //   scrollY: 300,
+  //   searching: false,
+  //   paging: false,
+  //   order: [[ 0, "desc" ],[ 3, "desc" ]],
+  //   bInfo: false
+  // });
   $('.all_nodes_dt').DataTable({
     scrollX: "100%",
-    scrollY: 500
+    scrollY: 500,
+    lengthMenu: [[25, 100, -1], [25, 100, "All"]]
   });
 });
 $('.dataTables_length').addClass('bs-select');
