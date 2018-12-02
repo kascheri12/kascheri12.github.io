67b635607f82eb3ddfedc5edc14ab6a91481611e
Kenneth Ascheri <ascherik@lanecc.edu>
2018-12-01 20:09:38 -0800

    Update main js, syntax fix

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $('.top_dt').DataTable({
     scrollX: true,
-    scrollY: 200
-    ,searching:false
-    ,paging:simple
+    scrollY: 200,
+    searching: false,
+    paging: false
   });
 });
 $('.dataTables_length').addClass('bs-select');
