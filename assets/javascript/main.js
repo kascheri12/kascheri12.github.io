a5838ef3543e041565a4b21416a2f20386abd070
Kenneth Ascheri <ascherik@lanecc.edu>
2018-12-02 11:56:49 -0800

    Add code back to js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  // $('.top_dt').DataTable({
-  //   scrollX: "100%",
-  //   scrollY: 300,
-  //   searching: false,
-  //   paging: false,
-  //   order: [[ 0, "desc" ],[ 3, "desc" ]],
-  //   bInfo: false
-  // });
+  $('.top_dt').DataTable({
+    scrollX: "100%",
+    scrollY: 300,
+    searching: false,
+    paging: false,
+    order: [[ 0, "desc" ],[ 3, "desc" ]],
+    bInfo: false
+  });
   $('.all_nodes_dt').DataTable({
     scrollX: "100%",
     scrollY: 500,
