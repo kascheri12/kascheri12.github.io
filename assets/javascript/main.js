9ed57b4674f2de5fbd66c33a26d3b260a9deaa18
Kenneth Ascheri <ascherik@lanecc.edu>
2018-12-01 20:24:04 -0800

    Make y scrolling shorter

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('.top_dt').DataTable({
     scrollX: true,
-    scrollY: 600,
+    scrollY: 300,
     searching: false,
     paging: false,
     order: [[ 0, "desc" ]],
