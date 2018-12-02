e6f185f97118ffb5655a6f671d6ec7f09d8c9bf9
Kenneth Ascheri <ascherik@lanecc.edu>
2018-12-01 20:07:51 -0800

    customize datatables. remove extras

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   $('.top_dt').DataTable({
     scrollX: true,
     scrollY: 200
+    ,searching:false
+    ,paging:simple
   });
 });
 $('.dataTables_length').addClass('bs-select');
