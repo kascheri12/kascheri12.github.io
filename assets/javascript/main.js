16a813a6e8d911a1a6f914ff73574871dde350e4
Kenneth Ascheri <ascherik@lanecc.edu>
2018-12-05 22:18:59 -0800

    Changes to main js for speed increase

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,20 +1,36 @@
+if (!window.location.origin) {
+  window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
+}
+
 $(document).ready(function() {
-  $('.top_dt').DataTable({
-    scrollX: "100%",
-    scrollY: 300,
-    searching: false,
-    paging: false,
-    order: [[ 0, "desc" ],[ 3, "desc" ]],
-    bInfo: false
-  });
-  $('#perc-change-subtasks-success').DataTable({
-    scrollX: "100%",
-    scrollY: 300,
-    searching: false,
-    paging: false,
-    order: [[ 0, "desc" ]],
-    bInfo: false
-  });
+  setTimeout(
+    function() {
+      var host = window.location.origin;
+      $('#iframe_nodes_connected_by_date').attr('src',host+'/graphs/nodes_connected_by_date');
+      $('#iframe_top_50_subtasks_success_by_date').attr('src',host+'/graphs/top_50_subtasks_success_by_date');
+      $('#iframe_golem-network').attr('src',host+'/graphs/golem-network');
+      $('#iframe_avg_daily_subtasks_totals').attr('src',host+'/graphs/avg_daily_subtasks_totals');
+      $('#iframe_avg_daily_failed_totals').attr('src',host+'/graphs/avg_daily_failed_totals');
+      $('#iframe_avg_daily_unique_totals').attr('src',host+'/graphs/avg_daily_unique_totals');
+      $('#iframe_new_unique_node_count_per_snapshot').attr('src',host+'/graphs/new_unique_node_count_per_snapshot');
+
+      $('.top_dt').DataTable({
+      scrollX: "100%",
+      scrollY: 300,
+      searching: false,
+      paging: false,
+      order: [[ 0, "desc" ],[ 3, "desc" ]],
+      bInfo: false
+      });
+      $('#perc-change-subtasks-success').DataTable({
+      scrollX: "100%",
+      scrollY: 300,
+      searching: false,
+      paging: false,
+      order: [[ 0, "desc" ]],
+      bInfo: false
+      });
+    }, 500);
   $('.all_nodes_dt').DataTable({
     scrollX: "100%",
     scrollY: 500,
